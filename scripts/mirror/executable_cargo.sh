f955ec1d010751c337a240796317d6e2682cb257
Undefined01 <amoscr@163.com>
2022-10-26 17:00:26 +0800

    Add executable attribution

--- scripts/mirror/executable_cargo.sh
+++ scripts/mirror/executable_cargo.sh
@@ -4,13 +4,13 @@ set -e
 
 if [ ! -f "$HOME/.cargo/config" ]; then
     cat >"$HOME/.cargo/config" <<EOF
-replace-with = "ustc"
-
 [source.crates-io]
 registry = "https://github.com/rust-lang/crates.io-index"
+replace-with = 'ustc'
 
 [source.ustc]
-registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+registry = "https://mirrors.ustc.edu.cn/crates.io-index"
+
 EOF
 else
     echo "$HOME/.cargo/config" already exists. You should edit it manually
