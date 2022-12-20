51a4d2deac2ff358fb860d7b1615cf3bc4eb251f
Undefined01 <amoscr@163.com>
2022-12-20 14:11:38 +0800

    Add arch settings

---
+++ scripts/install/executable_arch-init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+source $(dirname $0)/../utils.sh
+
+pacman -S --needed --noconfirm intel-ucode
+pacman -S --needed --noconfirm adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts firefox
+pacman -S --needed --noconfirm xfce4 xfce4-systemload-plugin
+
+pacman -S --needed --noconfirm zsh wget curl vim git tldr bottom dust ripgrep jq
+pacman -S --needed --noconfirm ntfs-3g
+
+
