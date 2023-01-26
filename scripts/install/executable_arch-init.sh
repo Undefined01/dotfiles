8113a081c238094a0f49b4efae3a58e7244e6eac
Undefined01 <amoscr@163.com>
2023-01-26 11:33:15 +0800

    Update config

--- scripts/install/executable_arch-init.sh
+++ scripts/install/executable_arch-init.sh
@@ -5,9 +5,9 @@ source $(dirname $0)/../utils.sh
 pacman -S --needed --noconfirm intel-ucode
 pacman -S --needed --noconfirm adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts firefox
 pacman -S --needed --noconfirm xfce4 xfce4-systemload-plugin
-pacman -S --needed --noconfirm fcitx5-im fcitx5-rime rime-luna-pinyin rime-double-pinyin rime-emoji
+pacman -S --needed --noconfirm fcitx5-im fcitx5-rime
 
-pacman -S --needed --noconfirm zsh wget curl vi vim git tldr bottom dust ripgrep jq
+pacman -S --needed --noconfirm zsh wget curl vim git tldr dust ripgrep jq mcfly
 pacman -S --needed --noconfirm ntfs-3g
-
+pacman -S --needed --noconfirm p7zip tumbler ffmpegthumbnailer parole gst-libav
 
