135fce23fd76cecd3be018ac9a3fbd85b557a866
Undefined01 <amoscr@163.com>
2023-05-13 10:17:45 +0000

    Update

--- scripts/install/executable_arch-init.sh
+++ scripts/install/executable_arch-init.sh
@@ -5,17 +5,26 @@ sed -i -E '/PasswordAuthentication (no|yes)$/c\PasswordAuthentication no' /etc/s
 systemctl enable sshd
 systemctl start sshd
 
-
 (cat /etc/group | grep sudo) || groupadd sudo
 (id -nG $(whoami) | grep -qw sudo) || usermod -a -G sudo lh
 sed -i -E '/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/c\%sudo ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
 
+
+# cli
+pacman -S --needed --noconfirm zsh wget curl vi vim git man tldr zip unzip dust ripgrep jq fzf
+# tmux
+pacman -S --needed --noconfirm zellij
+# mcfly
+
+# basic
 pacman -S --needed --noconfirm intel-ucode
-pacman -S --needed --noconfirm adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts firefox
-pacman -S --needed --noconfirm xfce4 xfce4-systemload-plugin
+pacman -S --needed --noconfirm amd-ucode
+pacman -S --needed --noconfirm ntfs-3g
+
+# desktop
+pacman -S --needed --noconfirm adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
+pacman -S --needed --noconfirm xfce4 xfce4-systemload-plugin firefox
+pacman -S --needed --noconfirm p7zip tumbler ffmpegthumbnailer parole gst-libav
 pacman -S --needed --noconfirm fcitx5-im fcitx5-rime
 
-pacman -S --needed --noconfirm zsh wget curl vi vim git man tldr zip unzip dust ripgrep jq fzf mcfly
-pacman -S --needed --noconfirm ntfs-3g
-pacman -S --needed --noconfirm p7zip tumbler ffmpegthumbnailer parole gst-libav
 
